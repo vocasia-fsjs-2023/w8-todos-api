6093e8f134994fb3cae3a4f081b5ffd3ecca3095
nishly <adhipamungkaswijayadi@gmail.com>
2023-10-07 16:36:34 +0700

    created todo model

---
+++ models/todo.js
@@ -0,0 +1,24 @@
+import { Model } from "sequelize";
+import db from "./index.js";
+
+export default class Todo extends Model {}
+
+Todo.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    status: {
+      type: DataTypes.ENUM,
+      values: ["created", "in_progress", "done"],
+    },
+  },
+  {
+    sequelize,
+    modelName: "Todo",
+  }
+);
