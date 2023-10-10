6dab7cb5182d8892b3a6f00d05a1ba13c51077c4
romli <mohammadromli098@gmail.com>
2023-10-11 04:38:13 +0700

    Weak 8 done

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const todoController = require("../controllers/todo-controller");
+const router = express.Router();
+
+router.post("/", todoController.store);
+router.get("/", todoController.index);
+router.get("/:id", todoController.get);
+router.route("/:id").put(todoController.update).patch(todoController.update);
+router.put("/:id", todoController.updateTodo);
+router.patch("/:id", todoController.updateStatusTodo);
+router.delete("/:id", todoController.remove);
+
+module.exports = router;
