4159be6c2045ea54bfbd3c590a43e1ca7a8ec98f
zulfaapril25 <142382739+zulfaapril25@users.noreply.github.com>
2023-10-11 22:05:35 +0700

    update tugas week 8
    mengumpulkan tugas week 8

---
+++ migrations/20231009124316-create-todos.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        max : 255
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.ENUM('created', 'in_progress', 'done'),
+        defaultValues : [
+          "created"
+        ],
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Todos');
+  }
+};
